3315ec3597aa70f1bf0c2776be4522ea8ab0dd70
valentinconan <valentin.conan@me.com>
2025-05-10 14:31:23 +0200

    add route for retry case

--- src/api/api.module.ts
+++ src/api/api.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
-import { HealthService } from './health/service/health.service';
-import { HealthController } from './health/controller/health.controller';
+import {Module} from '@nestjs/common';
+import {HealthService} from './health/service/health.service';
+import {HealthController} from './health/controller/health.controller';
 import {CustomerController} from "./customer/controller/customer.controller";
 import {RateLimitService} from "../service/rate-limit.service";
+import {RateLimitRetryService} from "../service/rate-limit-retry.service";
 
 @Module({
-  providers: [HealthService, RateLimitService],
-  controllers: [HealthController, CustomerController]
+    providers: [HealthService, RateLimitService, RateLimitRetryService],
+    controllers: [HealthController, CustomerController]
 })
-export class ApiModule {}
+export class ApiModule {
+}
--- src/api/customer/controller/customer.controller.ts
+++ src/api/customer/controller/customer.controller.ts
@@ -1,10 +1,11 @@
 import {Controller, Get, HttpCode} from "@nestjs/common";
 import {RateLimitService} from "../../../service/rate-limit.service";
+import {RateLimitRetryService} from "../../../service/rate-limit-retry.service";
 
 @Controller("/customer")
 export class CustomerController {
 
-    constructor(private readonly rateLimitService: RateLimitService) {
+    constructor(private readonly rateLimitService: RateLimitService, private readonly rateLimitRetryService: RateLimitRetryService) {
     }
 
     @Get()
@@ -16,6 +17,13 @@ export class CustomerController {
     @HttpCode(200)
     async rateLimit() {
         await this.rateLimitService.canMakeRequest();
+        return "done"
+    }
+
+    @Get("/rate-limit-retry")
+    @HttpCode(200)
+    async rateLimitRetry() {
+        await this.rateLimitRetryService.canMakeRequest();
         return "done"
     }
 }
